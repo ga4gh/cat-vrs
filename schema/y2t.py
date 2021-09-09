1fe0ff44602810919bf1043ea4c4da643eb4101f
Alex Handler Wagner <Alex.Wagner@nationwidechildrens.org>
2021-09-08 23:15:11 -0400

    WIP: yaml2text

---
+++ schema/y2t.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""convert yaml on stdin to .rst artifacts"""
+
+import yaml
+import os
+import pathlib
+from inflector import Inflector
+
+SECTION_DELIMITER = '#'
+
+defs_path = pathlib.Path.cwd() / 'defs'
+os.mkdir(defs_path)  # error expected if directory already exists – clear with Make
+
+with open('vrsatile.yaml', 'r') as f:
+    schema = yaml.load(f, Loader=yaml.SafeLoader)
+
+i = Inflector()
+
+
+def resolve_curie(curie):
+    namespace, identifier = curie.split(':')
+    base_url = schema['namespaces'][namespace]
+    return base_url + identifier
+
+
+def resolve_type(class_property_definition):
+    if 'type' in class_property_definition:
+        return class_property_definition['type']
+    elif '$ref_curie' in class_property_definition:
+        curie = class_property_definition['$ref_curie']
+        identifier = curie.split(':')[-1]
+        return f'`{identifier}<{resolve_curie(curie)}>`_'
+    elif '$ref' in class_property_definition:
+        ref = class_property_definition['$ref']
+        identifier = ref.split('/')[-1]
+        return f'`{identifier}<{ref}>`_'
+    elif 'oneOf' in class_property_definition:
+        return ' | '.join([resolve_type(x) for x in class_property_definition['oneOf']])
+    else:
+        raise ValueError(class_property_definition)
+
+def resolve_cardinality(class_property_name, class_definition):
+    """Resolve class property cardinality from yaml definition"""
+    raise NotImplementedError
+
+
+for class_name, class_definition in schema['$defs'].items():
+    with open(defs_path / (class_name + '.rst'), "w") as f:
+        header = i.titleize(class_name)
+        ref = i.underscore(class_name)
+        print(f'.. _{ref}:\n', file=f)
+        print(header, file=f)
+        print(SECTION_DELIMITER * len(header), file=f)
+        print(class_definition['description'], file=f)
+        print("""
+.. list-table::
+   :class: clean-wrap
+   :header-rows: 1
+   :align: left
+   :widths: auto
+   
+   *  - Field
+      - Type
+      - Limits
+      - Description""", file=f)
+        if 'heritable_properties' in class_definition:
+            p = 'heritable_properties'
+        elif 'properties' in class_definition:
+            p = 'properties'
+        else:
+            raise ValueError(class_name)
+        for class_property_name, class_property_attributes in class_definition[p].items():
+            print(f"""\
+   *  - {class_property_name}
+      - {resolve_type(class_property_attributes)}
+      - {resolve_cardinality(class_property_name, class_definition)}
+      - MUST be "VariationDescriptor""", file=f)
